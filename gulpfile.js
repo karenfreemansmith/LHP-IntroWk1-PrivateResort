5193eede975771a9b0f3a47e9aebce1dd4fc3b89
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-06-28 08:07:09 -0700

    add browser-sync and test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var sync = require('browser-sync');
 
 gulp.task('default', function() {
   console.log('hello from gulp');
+});
+
+gulp.task('serve', function() {
+  sync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
 });
