38d8e78d233484135f332d8cdf2d198d4356f052
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-06-28 08:41:59 -0700

    add gulp-sass and test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sync = require('browser-sync');
+var sass = require('gulp-sass');
+var maps = require('gulp-sourcemaps');
 
 gulp.task('default', function() {
   console.log('hello from gulp');
@@ -12,4 +14,22 @@ gulp.task('serve', function() {
       index: "index.html"
     }
   });
+  gulp.watch(['*.html'],['htmlBuild']);
+  gulp.watch(['scss/*.scss'],['scssBuild']);
+});
+
+gulp.task('htmlBuild', function() {
+  sync.reload();
+});
+
+gulp.task('scssBuild', ['scssCSS'], function() {
+  sync.reload();
+});
+
+gulp.task('scssCSS', function() {
+  return gulp.src('./scss/*.scss')
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(maps.write())
+    .pipe(gulp.dest('./css'));
 });
